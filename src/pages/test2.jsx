c4e6925661b2a1b1331b99eedd5f8d85ec570f41
Tobias Fenner <tfenner@usc.edu>
2023-11-20 08:00:25 -0800

    Update test2.jsx

--- src/pages/test2.jsx
+++ src/pages/test2.jsx
@@ -29,7 +29,7 @@ const Test2 = () => {
           <NavBar active="test2" />
           <div className="content-wrapper" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <div style={{ border: '2px solid red', padding: '20px', boxShadow: '3px 3px 5px rgba(0, 0, 0, 0.2)' }}>
-              Test page 1: Timeout
+              Test page: Timeout
             </div>
           </div>
         </div>
