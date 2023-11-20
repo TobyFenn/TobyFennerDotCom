99cb6d1a9964b8ce99aeb585d16d9f1dcde5bc9a
Tobias Fenner <tfenner@usc.edu>
2023-11-20 07:56:58 -0800

    Update testredirect.js

--- api/testredirect.js
+++ api/testredirect.js
@@ -1,7 +1,4 @@
 // api/test4.js
 module.exports = (req, res) => {
-    // Redirect to the homepage and set a 302 (temporary) redirect status
-    res.writeHead(302, { Location: '/' });
-    res.end();
-  };
-  
+  res.redirect(302, '/');
+};
