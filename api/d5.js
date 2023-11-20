81ac02a175c7b1085eb20c8e85fbdb33813c0302
Tobias Fenner <tfenner@usc.edu>
2023-11-19 19:38:13 -0800

    d3, d5, d10

---
+++ api/d5.js
@@ -0,0 +1,7 @@
+// delay.js
+module.exports = (req, res) => {
+  const delayInSeconds = 5; // Adjust the delay time as needed (in seconds)
+  setTimeout(() => {
+    res.status(200).send(`Delayed response for ${delayInSeconds} seconds.`);
+  }, delayInSeconds * 1000);
+};
